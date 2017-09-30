a366d70a08a69d8cae0ad0a3cb905a4b1fdc7321
billcyz <billcyz@gmail.com>
2017-09-30 23:17:52 +1300

    Judge Route solution

--- JavaBasement/src/leetCode/JudgeRoute_Solution.java
+++ JavaBasement/src/leetCode/JudgeRoute_Solution.java
@@ -1,6 +1,6 @@
 package leetCode;
 
-// logic is wrong
+import java.util.Scanner;
 
 public class JudgeRoute_Solution {
 	static int up = 1; // Up
@@ -8,29 +8,37 @@ public class JudgeRoute_Solution {
 	static int right = 1; // Right
 	static int left = -1; // Left
 	
+	static int x, y = 0;
+	
 	public static void main(String[] args) {
-		int result = 0;
 		boolean circle = true;
-		String[] moves = {"U", "D", "R", "D"};
 		
-		for(int i = 0; i < moves.length; i++) {
-			
-			if(moves[i] == "U") {
-				result += up;
-			} else if(moves[i] == "D") {
-				result += down;
-			} else if(moves[i] == "R") {
-				result += right;
-			} else if(moves[i] == "L") {
-				result += left;
+		System.out.println("Please enter route: ");
+		Scanner sc = new Scanner(System.in);
+		String moves = sc.nextLine();
+		
+		sc.close();
+		
+		// process each char in string
+		for(char c : moves.toCharArray()) {
+			if(c == 'U') {
+				y += up;
+			} else if(c == 'D') {
+				y += down;
+			} else if(c == 'L') {
+				x += left;
+			} else if(c == 'R') {
+				x += right;
 			}
-			
 		}
+
 		
-		if(result == 0) {
+		if(x == 0 && y == 0) {
 			System.out.println(circle);
 		} else {
+			System.out.println("x is: " + x + " y is: " + y);
 			System.out.println(circle = false);
 		}
+		
 	}
 }

30d8017fccc8dd0d869a2fcdc27a773a7ecd8b4f
billcyz <billcyz@gmail.com>
2017-10-01 20:56:40 +1300

    Valid number problem

---
+++ JavaBasement/src/leetCode/ValidNumber_solution.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+//Validate if a given string is numeric.
+//"0" => true
+//" 0.1 " => true
+//"abc" => false
+//"1 a" => false
+//"2e10" => true
+
+public class ValidNumber_solution {
+
+}
+
+class ValidNumberSolution {
+    public boolean isNumber(String s) {
+        
+    }
+}

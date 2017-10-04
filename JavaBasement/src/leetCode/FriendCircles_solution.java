dad198355a60ebc141eae7b15c175ed86be25349
billcyz <billcyz@gmail.com>
2017-10-04 17:46:13 +1300

    friend circle problem

---
+++ JavaBasement/src/leetCode/FriendCircles_solution.java
@@ -0,0 +1,13 @@
+package leetCode;
+
+// Problem link: https://leetcode.com/problems/friend-circles/description/
+
+public class FriendCircles_solution {
+
+}
+
+class FriendCircleSolution {
+    public int findCircleNum(int[][] M) {
+        
+    }
+}
